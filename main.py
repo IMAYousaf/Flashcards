fbf270a0e0bb5b2cf83e935cd8498768ce53a827
IMAYousaf <IMAYousaf@gmail.com>
2016-12-24 15:24:49 -0800

    Cleaned Up main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-# Eventually make a Flashcard App.
-# Be as abusive as possible.
-# Add more controls and functionality.
-# Keep "score".
-# Add in multiple controls and score tracking.
 
 import random
 
@@ -15,7 +10,6 @@ def main():
         if selection in {"END", "I QUIT"}:
             print("Way to take the easy way out. It's only simple math!")
             quit()
-
         while x + y >= 100:
             x = random.randint(9, 100)
             y = random.randint(9, 100)
@@ -38,15 +32,5 @@ def main():
                 print("You got the right answer!")
             else:
                 print("Learn how to add idiot.")
-
-
-
-'''
-def start_selection():
-    print("Please choose your difficulty.")
-    print("Enter 1 if you want numbers between 1 and 10.")
-    print("Enter 2 if you want numbers between 1 and 100.")
-    #difficulty = input("Please enter your desired difficulty: ")
-'''
 
 main()
